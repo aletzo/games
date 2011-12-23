42e7b929fca3533848692f5ed0da3525fa840810
ale tzo <ajo@mydevnull.net>
2011-12-23 19:32:57 +0200

    improved the appearance and added game instructions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-$('a.game').colorbox({
-    iframe: true,
-    innerHeight: 500,
-    innerWidth: 500,
-    opacity: 0.3
+$('#accordion').accordion({
+    change: function(ev, ui) {
+        $('a.questionmark').each(function(i, el) {
+            if ( ! $(el).is(':visible')) {
+                $(el).siblings('div.help').hide();
+            }
+        });
+    }
+});
+
+$('a.questionmark').click(function() {
+    $(this).siblings('div.help').fadeToggle(300);
+
+    return false;
 });
